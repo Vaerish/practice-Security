a471e5acb3b175df5876c29f4fadf8090cca5919
jfhn6 <jfhn6@mst.edu>
2019-11-24 00:18:48 +0000

    Update decodeSys.py

--- decodeSys.py
+++ decodeSys.py
@@ -3,6 +3,9 @@ import crypt
 
 SysPass = open("Decoded.txt", "r")
 contents = SysPass.read()
-contents = contents.split('\n')
+contents = contents.split('=>')
+contents = contents[-1]
+contents = contents.split('/n')
 contents = contents[0].split(':')
-print(contents[1])
+contents = contents[1].split()
+print(contents[0])
