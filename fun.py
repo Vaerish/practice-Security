22d4e335268590bdd44729c461f70b02f9faa2ad
Joelle Frye <jfhn6@mst.edu>
2019-12-11 16:37:21 -0600

    fixed it by moving stuff to python as bash is weird

--- fun.py
+++ fun.py
@@ -1,7 +1,21 @@
 import sys
 import crypt
 
-to_hash = sys.argv[1]
-to_salt = "$6$"+sys.argv[2]
-password = crypt.crypt(to_hash, to_salt)
-print(password)
+def to_hash_fun(var1, var2):
+	to_hash = var1
+	to_salt = "$6$"+var2
+	password = crypt.crypt(to_hash, to_salt)
+	return password
+
+dictionary = sys.argv[1]
+hashBoss = sys.argv[2]
+saltBoss = sys.argv[3]
+
+dictionary = open(dictionary,"r")
+dictionary = dictionary.read()
+newDictionary = dictionary.split()
+for word in newDictionary:
+	test=to_hash_fun(word, saltBoss)
+	if(test == hashBoss):
+		print(word)
+		break
--- funandgames.sh
+++ funandgames.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 TempWorker=correctbatteryhorsestaple99
 TempWorkerHashed=$(grep "tempworker.*\n" /etc/shadow)
-YourBoss=$(grep "yourboss.*\n" /etc/shadow)
-SysAdmin=$(grep "sysadmin.*\n" /etc/shadow)
+filename='/etc/shadow'
+while read line; do
+	if [[ $line =~ "yourboss" ]]
+	then
+		bossStuff=$line
+	fi
+	if [[ $line =~ "sysadmin" ]]
+	then
+		SysAdmin=$line
+	fi
+done < $filename
 echo $SysAdmin > SysPassword.txt
 touch Decoded.txt
-TempWorkerHashed=${TempWorkerHashed:0}
-TempWorkerHashed=${TempWorkerHashed:0:-19}
-YourBoss=${YourBoss:9:-19}
+IFS=':'
+read -ra ADDR <<< "$bossStuff"
+IFS='$'
+read -ra ADDR2 <<< "${ADDR[1]}"
+IFS=''
+bossSalt=${ADDR2[2]}
+bossHash="$"${ADDR2[1]}"$"$bossSalt"$"${ADDR2[3]}
 DecryptedSys=$( hashcat --force -m 1800 SysPassword.txt Decoded.txt /usr/share/dict/american-english-small > Decoded.txt)
 DecryptedPassSys=$(python3 decodeSys.py)
 #Finished up making it just the password section now we just need to figure out salt
-SaltTemp=$YourBoss
-#Then encode every word in dictionary with salt to find word
-IsPassword=password
-SaltTemp=${SaltTemp:3:8}
-
-IsPassword=$(python3 fun.py correctbatteryhorsestaple99 g1VamdqE)
-IsPassword=${IsPassword}
-
-for j in $(cat /usr/share/dict/american-english-small)
-do
-	IsPassword=$j
-	IsPassword=$(python3 fun.py $j $SaltTemp)
-	if [ $IsPassword == $YourBoss ]; then
-		echo $j
-		break
-	fi
-done
-
+BossPassword=$(python3 fun.py '/usr/share/dict/american-english' $bossHash $bossSalt)
+echo $BossPassword
 #now I need to use john to break the sysadmins password
 echo $TempWorker
 echo $DecryptedPassSys
